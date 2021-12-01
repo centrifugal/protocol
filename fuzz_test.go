1bc0f9188bf75cb870fa78069316bc5e9c928984
Alexander Emelin <frvzmb@gmail.com>
2021-12-01 03:28:12 +0300

    fuzz json with different corpus

--- fuzz_test.go
+++ fuzz_test.go
@@ -7,11 +7,29 @@ import (
 	"testing"
 )
 
-func FuzzJSONDecode(f *testing.F) {
+func FuzzJSONDecodeSingle(f *testing.F) {
 	f.Add([]byte(`{"id": 1, "method": "", "params": {}}`))
 	f.Fuzz(func(t *testing.T, b []byte) {
 		decoder := GetCommandDecoder(TypeJSON, b)
 		_, err := decoder.Decode()
+		if err != nil {
+			t.Skip()
+		}
+		PutCommandDecoder(TypeJSON, decoder)
+	})
+}
+
+func FuzzJSONDecodeMultiple(f *testing.F) {
+	f.Add([]byte(`{"id": 1, "method": "", "params": {}}
+{"id": 2, "method": "", "params": {}}
+`))
+	f.Fuzz(func(t *testing.T, b []byte) {
+		decoder := GetCommandDecoder(TypeJSON, b)
+		_, err := decoder.Decode()
+		if err != nil {
+			t.Skip()
+		}
+		_, err = decoder.Decode()
 		if err != nil {
 			t.Skip()
 		}
